2de2f2889c585b3eeaac73cf7d1a96ec373bcdcd
zj27 <zj27jk@163.com>
2016-06-24 11:38:11 +0800

    Finish exercise 1.12

---
+++ ch1/e1-12.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
+
+const (
+	blackIndex = 0 // first color in palette
+	greenIndex = 1 // next color in palette
+)
+
+func main() {
+	http.HandleFunc("/", handler) // each request calls handler
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the Path component of the request URL r.
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	// fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	cycles := 5
+	if v, ok := r.URL.Query()["cycles"]; ok {
+		c, err := strconv.Atoi(v[0])
+		if err == nil {
+			cycles = c
+		}
+	}
+	lissajous(w, cycles)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func lissajous(out io.Writer, cycles int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}

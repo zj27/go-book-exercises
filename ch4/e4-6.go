1568f53847da685664ffe97b893f09f48fd657fc
zj27 <zj27jk@163.com>
2016-07-22 15:14:47 +0800

    Refine 4.6 and 4.7

--- ch4/e4-6.go
+++ ch4/e4-6.go
@@ -26,4 +26,9 @@ func main() {
 	data := []byte{'a', ' ', 'b', ' ', ' ', 'c', '\t', 'd', ' ', ' ', '\t', '\t', 'e'}
 	fmt.Printf("%q\n", data)
 	fmt.Printf("%q\n", dedupSpace(data))
+
+	s := "一  二	 三"
+	d := []byte(s)
+	fmt.Printf("%q\n", d)
+	fmt.Printf("%q\n", dedupSpace(d))
 }
--- ch4/e4-7.go
+++ ch4/e4-7.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func reverse(s []byte) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
@@ -8,8 +11,22 @@ func reverse(s []byte) {
 	}
 }
 
+func reverseUTF8(s []byte) {
+	for i, j := 0, 1; j <= len(s); j++ {
+		if utf8.Valid(s[i:j]) {
+			reverse(s[i:j])
+			i = j
+		}
+	}
+	reverse(s)
+}
+
 func main() {
 	data := []byte{'a', 'b', 'c'}
-	reverse(data[:])
+	reverseUTF8(data[:])
 	fmt.Printf("%q\n", data)
+	s := "一二三"
+	d := []byte(s)
+	reverseUTF8(d)
+	fmt.Printf("%q\n", d)
 }

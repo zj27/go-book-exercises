19e28602bbe7793ee3fdf8d259d0b043d71d1cab
zj27 <zj27jk@163.com>
2016-07-25 16:20:35 +0800

    Finish exercise 4.10

---
+++ ch4/e4-10.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/zj27/go-book-exercises/ch4/github"
+)
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	var cat_1m []*github.Issue
+	var cat_1y []*github.Issue
+	var cat_more []*github.Issue
+
+	now := time.Now()
+	for _, item := range result.Items {
+		d := now.Sub(item.CreatedAt)
+		if d.Hours() < 24.0*30 {
+			cat_1m = append(cat_1m, item)
+		} else if d.Hours() < 24.0*365 {
+			cat_1y = append(cat_1y, item)
+		} else {
+			cat_more = append(cat_more, item)
+		}
+	}
+
+	fmt.Printf("\nWithin a month\n")
+	for _, item := range cat_1m {
+		fmt.Printf("#%-5d %9.9s %s %.55s\n",
+			item.Number, item.User.Login, item.CreatedAt.String(), item.Title)
+	}
+
+	fmt.Printf("\nWithin a year\n")
+	for _, item := range cat_1y {
+		fmt.Printf("#%-5d %9.9s %s %.55s\n",
+			item.Number, item.User.Login, item.CreatedAt.String(), item.Title)
+	}
+
+	fmt.Printf("\nMore than a year\n")
+	for _, item := range cat_more {
+		fmt.Printf("#%-5d %9.9s %s %.55s\n",
+			item.Number, item.User.Login, item.CreatedAt.String(), item.Title)
+	}
+
+}

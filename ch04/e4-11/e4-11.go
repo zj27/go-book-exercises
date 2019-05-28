f6aa5b46264e4d106599385e9ef97e81462c38d0
zj27 <zj27jk@163.com>
2019-05-28 16:45:20 +0800

    e04-11: Add create issue with default title and body

--- ch04/e4-11/e4-11.go
+++ ch04/e4-11/e4-11.go
@@ -9,40 +9,73 @@ import (
 	"github.com/zj27/go-book-exercises/ch04/github"
 )
 
+var user = "zj27"
+var repo = "helloworld"
+
 func main() {
 	listMode := flag.Bool("list", false, "List issues")
 	readMode := flag.Bool("read", false, "Read a issue")
+	createMode := flag.Bool("create", false, "Create a issue")
 	issueID := flag.Int("id", -1, "ID of the issue to read")
+	username := flag.String("username", "", "Username for auth")
+	token := flag.String("token", "", "Token for auth")
 
 	flag.Parse()
 
-	user, repo := "zj27", "helloworld"
 	if *listMode {
-		result, err := github.ListIssues(user, repo)
-		if err != nil {
-			log.Fatal(err)
-		}
-		//	fmt.Printf("%d issues:\n", result.TotalCount)
-
-		for _, item := range result {
-			fmt.Printf("#%-5d %9.9s %.55s\n",
-				item.Number, item.User.Login, item.Title)
-		}
+		ListIssues()
 	} else if *readMode {
-		item, err := github.ReadIssue(user, repo, strconv.Itoa(*issueID))
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("#%d\n", item.Number)
-		fmt.Printf("Title:\t%s\n", item.Title)
-		fmt.Printf("URL:\t%s\n", item.HTMLURL)
-		fmt.Printf("User:\t%s\t%s\n", item.User.Login, item.User.HTMLURL)
-		fmt.Printf("State:\t%s\n", item.State)
-		fmt.Printf("CreateTime:\t%s\n", item.CreatedAt)
-		fmt.Printf("Body:\n%s\n", item.Body)
+		ReadIssue(issueID)
+	} else if *createMode {
+		CreateIssue(username, token)
 	} else {
 		fmt.Println("Usage:")
 		flag.PrintDefaults()
 	}
 
+}
+
+func ListIssues() {
+	result, err := github.ListIssues(user, repo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	for _, item := range result {
+		fmt.Printf("#%-5d %9.9s %.55s\n",
+			item.Number, item.User.Login, item.Title)
+	}
+
+}
+
+func ReadIssue(issueID *int) {
+	item, err := github.ReadIssue(user, repo, strconv.Itoa(*issueID))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("#%d\n", item.Number)
+	fmt.Printf("Title:\t%s\n", item.Title)
+	fmt.Printf("URL:\t%s\n", item.HTMLURL)
+	fmt.Printf("User:\t%s\t%s\n", item.User.Login, item.User.HTMLURL)
+	fmt.Printf("State:\t%s\n", item.State)
+	fmt.Printf("CreateTime:\t%s\n", item.CreatedAt)
+	fmt.Printf("Body:\n%s\n", item.Body)
+
+}
+
+func CreateIssue(username *string, token *string) {
+	title := "testTitle"
+	body := "testBody"
+	var newIssue github.IssueForWrite
+	newIssue.Title = title
+	newIssue.Body = body
+	var auth github.Auth
+	auth.Username = *username
+	auth.Password = *token
+	item, err := github.CreateIssue(user, repo, auth, &newIssue)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Issue #%d is created\n", item.Number)
 }
--- ch04/github/issues.go
+++ ch04/github/issues.go
@@ -1,10 +1,23 @@
 package github
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
+
+type Auth struct {
+	Username string
+	Password string
+}
+
+type IssueForWrite struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
 
 func ListIssues(user string, repo string) ([]*Issue, error) {
 	listIssuesURL := "https://api.github.com/repos/" + user +
@@ -36,6 +49,38 @@ func ReadIssue(user string, repo string, id string) (*Issue, error) {
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("Get issue failed: %s", resp.Status)
+	}
+
+	var result Issue
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
+
+func CreateIssue(user string, repo string, auth Auth, issue *IssueForWrite) (*Issue, error) {
+	createIssueURL := "https://api.github.com/repos/" + user +
+		"/" + repo + "/issues"
+
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(issue)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", createIssueURL, buf)
+	if err != nil {
+		return nil, fmt.Errorf("Create issue failed: %s", err)
+	}
+
+	req.SetBasicAuth(auth.Username, auth.Password)
+	resp, err := client.Do(req)
+
+	if resp.StatusCode != http.StatusCreated {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		return nil, fmt.Errorf("Create issue failed: %s\n %s", resp.Status, bodyString)
 	}
 
 	var result Issue
